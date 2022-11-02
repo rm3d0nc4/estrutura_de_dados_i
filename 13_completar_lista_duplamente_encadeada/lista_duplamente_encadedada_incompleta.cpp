c65e9a1a1f869cdb9719a16e42fd0842e8b6c4a7
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2022-11-02 09:32:42 -0300

    completed exercise 13

--- 13_completar_lista_duplamente_encadeada/lista_duplamente_encadedada_incompleta.cpp
+++ 13_completar_lista_duplamente_encadeada/lista_duplamente_encadedada_incompleta.cpp
@@ -30,37 +30,121 @@ class Lista{
 		}	
 
 		void addToFinal(int m, char n[23]){
+			No* novoNo = new No(m, n);
 			
+			if(fim == NULL) {
+				fim = novoNo;
+				inicio = novoNo;
+			} else {
+				fim->prox = novoNo;
+				novoNo->ant = fim;
+				fim = novoNo;
+			}
 		}
 		
 		void addToIni(int m, char n[23]){
-			
+			No* novoNo = new No(m, n);
+
+			if(inicio == NULL) {
+				fim = novoNo;
+				inicio = novoNo;
+			} else {
+				inicio->ant = novoNo;
+				novoNo->prox = inicio;
+				inicio = novoNo;
+
+			}
 		}
 		
 		void addOrd(int m, char n[23]){
+			No* novoNo = new No(m, n);
 			
+			if(inicio == NULL) {
+				inicio = novoNo;
+				fim = novoNo;
+
+			} else if(inicio == fim) {
+				if(m > inicio->mat) {
+					addToFinal(m, n);
+				} else {
+					addToIni(m,n);
+				}
+				free(novoNo);
+			} else {
+				No* atual = inicio;
+				while(atual->mat < m && atual->prox != NULL) {
+					atual = atual->prox;
+				}
+				novoNo->prox = atual;
+				novoNo->ant = atual->ant;
+				atual->ant->prox = novoNo;
+				atual->ant = novoNo;
+
+				atual = novoNo;
+			}
+			return;
 		}
 		
 		//mostra todos os elementos do inicio ao final da lista
 		void mostraIniFim(){
-			
+			if (inicio == NULL) cout<<"\nLista vazia!!!";
+			else {
+				No* elemento = inicio;
+				while (elemento != NULL) {
+					cout << elemento->mat << ": " << elemento->nome << endl;
+					elemento = elemento->prox;
+				}
+				free(elemento);
+				cout << endl;
+			}
 		}
 		
 		// mostra todos os elementos do final ao inicio da lista
 		void mostraFimIni(){
-			
+			if (fim == NULL) cout<<"\nLista vazia!!!";
+			else {
+				No* elemento = fim;
+				while (elemento != NULL) {
+					cout << elemento->mat << ": " << elemento->nome << endl;
+					elemento = elemento->ant;
+				}
+				free(elemento);
+				cout << endl;
+			}
 		}
 		
 		int removerFinal(){
-			
+			No* elemento = fim;
+			fim = fim->ant;
+			free(elemento);
+			return elemento->mat;
 		}
 		
 		int removerInicio(){
-			
+			No* elemento = inicio;
+			inicio = inicio->prox;
+			free(elemento);
+			return elemento->mat;
 		}
 
 		int remover(int m){
-			
+			if(m == inicio->mat) {
+				removerInicio();
+			} else if(m == fim->mat) {
+				removerFinal();
+			} else {
+				No* atual = inicio->prox;
+
+				while(atual->mat != m) {
+					if(atual->mat > m || atual == NULL) return 0;
+					else atual = atual->prox;
+				}
+				
+				atual->ant->prox = atual->prox;
+				atual->prox->ant = atual->ant;
+				free(atual);
+			}
+			return 1;
 		}
 };
 main(){
